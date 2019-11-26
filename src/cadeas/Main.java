c9fb000c0e453b3c25e14c2696157be1d5c34f7f
Miguelmc5 <mmartinezcosta@danielcastelao.org>
2019-11-26 10:59:38 +0100

    Initial commit

---
+++ src/cadeas/Main.java
@@ -0,0 +1,19 @@
+package cadeas;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+   /* string cadea1="daniel castelao";
+   string cadea2="impaga";
+   1- tamaño de la cadena
+   2- son = o distinta la cadena1 y la cadena2
+   3- caracter, posicion 5, de cadena2
+   4- concatena las cadenas
+   5- pasa cadena1 a minusculas
+   6- pasa cadena2 a mayusculas
+   */
+
+    }
+}
